cc80bdd83d30b50188f112ae88c746872ab851a7
dembar <dbarlaro@gmail.com>
2013-06-25 17:22:42 +0300

    Added OptionParser support and pidfile option

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import logging
 import signal
 import sys
 import yaml
+import os
 from plumbago import Plumbago
 
 __author__ = 'uzix'
@@ -10,16 +11,22 @@ __author__ = 'uzix'
 log = logging.getLogger()
 
 def main():
-    config_file = 'config.yaml'
-    if len(sys.argv) > 1:
-        config_file = sys.argv[1]
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("-p", "--pid-file", dest="pid", default='plumbago.pid', help="Plumbago pid file", metavar="PID_FILE")
+    parser.add_option("-c", "--config-file", dest="config", default='config.yaml', help="Plumbago config file", metavar="CONFIG_FILE")
+    (options, args) = parser.parse_args()
 
-    config = yaml.load(open(config_file, 'r'))
+    pidfile=open(options.pid, 'w')
+    pidfile.write(str(os.getpid()))
+    pidfile.close()
+
+    config = yaml.load(open(options.config, 'r'))
     server = Plumbago(config)
 
     def handler(signum, frame):
         if signum == signal.SIGUSR1:
-            config = yaml.load(open(config_file, 'r'))
+            config = yaml.load(open(options.config, 'r'))
             server.configure(config)
         elif signum == signal.SIGUSR2:
             server.dump_status()
